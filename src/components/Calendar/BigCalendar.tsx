89d1fc7a80b6da7a42656f8c23cee894a9de3d36
oysterjung <resd877@naver.com>
2023-05-16 19:32:02 +0900

    중간점검 겸 머지

--- src/components/Calendar/BigCalendar.tsx
+++ src/components/Calendar/BigCalendar.tsx
@@ -8,7 +8,6 @@ import dayjs from 'dayjs'
 import { Vacation, VacationStatus } from '../../types/vacation'
 import { instance } from '../../api/instance'
 
-<<<<<<< HEAD
 interface BigCalendarProps {
   localizer: DateLocalizer
   vacations: CustomEvent[]
@@ -25,92 +24,6 @@ interface BigCalendarProps {
   onSelect: (event: CustomEvent) => void
   onNavigate: (date: Date) => void
 }
-=======
-// const tmpEvents = [
-//   createCalendarEvent({ id: 1, title: '김아무(개발)', start: '2023-05-01', end: '2023-05-01', type: 'duty' }),
-//   createCalendarEvent({ id: 2, title: '박아무(영업)', start: '2023-05-09', end: '2023-05-09', type: 'duty' }),
-//   createCalendarEvent({ id: 3, title: '광아무(영업)', start: '2023-05-23', end: '2023-05-23', type: 'duty' }),
-//   createCalendarEvent({ id: 4, title: '조아무(인사)', start: '2023-05-04', end: '2023-05-06', type: 'vacation' }),
-//   createCalendarEvent({ id: 5, title: '최아무(마케팅)', start: '2023-05-12', end: '2023-05-18', type: 'vacation' }),
-//   createCalendarEvent({ id: 6, title: '이아무(마케팅)', start: '2023-05-20', end: '2023-05-22', type: 'vacation' }),
-// ]
-
-// function BigCalendar() {
-//   const tmpEvents: Vacation[] = [
-//     {
-//       id: 1,
-//       memberName: '김무무',
-//       departmentName: '개발',
-//       start: '2023-05-05',
-//       end: '2023-05-09',
-//       status: VacationStatus['WAITING'],
-//     },
-//     {
-//       id: 2,
-//       memberName: '박무무',
-//       departmentName: '개발',
-//       start: '2023-05-11',
-//       end: '2023-05-21',
-
-//       status: VacationStatus['WAITING'],
-//     },
-//   ]
-
-//   const localizer = dayjsLocalizer(dayjsInstance)
-//   const [dutys, setDutys] = useState<CostomEvent[]>([])
-//   const [vacations, setVacations] = useState<CostomEvent[]>([])
-
-//   const { data: duty, mutate: dutyMutate } = useMutation(
-//     async (date: string) => {
-//       //date담아보내기
-//       const res = await fetch('/api/v1/duty/list')
-//       return tmpEvents
-//     },
-//     {
-//       onSuccess: (data) => {
-//         const formattedData: CostomEvent[] = data.map((event) => ({
-//           title: event.memberName,
-//           start: new Date(event.start),
-//           end: new Date(event.start),
-//           status: event.status,
-//           type: 'duty',
-//         }))
-//         setDutys([...formattedData])
-//       },
-//     },
-//   )
-//   const { data: vacation, mutate: vacationMutate } = useMutation(
-//     async (date: string) => {
-//       //date담아보내기
-//       const res = await fetch('/api/v1/vacation/list')
-//       return tmpEvents
-//     },
-//     {
-//       onSuccess: (data) => {
-//         const formattedData: CostomEvent[] = data.map((event) => ({
-//           title: event.memberName,
-//           start: new Date(event.start),
-//           end: new Date(event.end),
-//           status: event.status,
-//           type: 'vacation',
-//         }))
-//         setVacations([...formattedData])
-//       },
-//     },
-//   )
-
-//   const onNavigate = async (date: Date) => {
-//     if (date) {
-//       const originDate = new Date(date)
-//       const formattedDate = dayjs(originDate).format('YYYY-MM-DD')
-
-//       await Promise.all([vacationMutate(formattedDate), dutyMutate(formattedDate)])
-//     }
-//   }
-//   onSelect: (event: CustomEvent) => void
-//   onNavigate: (date: Date) => void
-// }
->>>>>>> 2a81456a2a78f955fe144a1d9920898f3f7ac125
 
 function BigCalendar({ vacations, dutys, localizer, eventPropGetter, onSelect, onNavigate }: BigCalendarProps) {
   return (
