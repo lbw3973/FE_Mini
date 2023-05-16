c48158e404fac88b2cab05494456e915a5725bf9
bwlee <lbw3973@naver.com>
2023-05-16 17:45:57 +0900

    feat: mutate 성공 후 invalidateQueries 적용

--- src/hooks/useAcceptSignup.ts
+++ src/hooks/useAcceptSignup.ts
@@ -1,16 +1,15 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { acceptSignUp, rejectSignUp } from '../api/admin'
-import { useNavigate } from 'react-router-dom'
 import { AxiosError } from 'axios'
 
 export const useAcceptSignup = (Accept: boolean) => {
-  const navigate = useNavigate()
+  const queryClient = useQueryClient()
   const post = Accept ? acceptSignUp : rejectSignUp
 
   const { mutate } = useMutation(post, {
     onSuccess: (data) => {
       console.log(data)
-      // navigate('/admin/signup')
+      queryClient.invalidateQueries(['signUp'])
     },
     onError: (err: AxiosError) => {
       console.log(err)
